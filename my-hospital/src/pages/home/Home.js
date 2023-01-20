08ebb272f69ee55373effd50a4d81311447de515
nandweza <allannandweza@gmail.com>
2023-01-20 13:35:01 +0300

    created the homepage

--- my-hospital/src/pages/home/Home.js
+++ my-hospital/src/pages/home/Home.js
@@ -1,16 +1,82 @@
 import "./home.css";
+import { Link } from "react-router-dom";
+import {
+  Person,
+  AirlineSeatIndividualSuite,
+  LocalHospital,
+  LocalPharmacy,
+  Star,
+  Timeline,
+  DateRange,
+  MonetizationOn,
+} from "@material-ui/icons";
 
 export default function Home() {
+  const currentDate = new Date();
+  const today = new Date();
+
+  const date = `${currentDate.getDate()}.${currentDate.getMonth()+1}.${currentDate.getFullYear()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
   return (
     <div className="home">
       <div className="container">
-        <h1>
-          Welcome to myHospital
-        </h1>
-        <br/>
-        <p>
-          Manage all your hospital data with ease in a glance...
-        </p>
+        <div className="wrapper">
+          <div className="row1">
+            <div className="upper reports">
+              <Timeline className="icon" />
+              <h3>Analysis<br/>& Report</h3>
+            </div>
+            <div className="upper invetory">
+              <MonetizationOn className="icon" />
+              <h3>$10,000</h3>
+              <h3>Invetory</h3>
+            </div>
+            <div className="upper dates">
+              < DateRange className="icon" />
+              <h2>{time}</h2>
+              <p>{date}</p>
+            </div>
+          </div>
+          <div className="row2">
+            <div className="person">
+              <Person className="icon" />
+              <h4>3</h4>
+              <h3>Doctors</h3>
+            </div>
+            <div className="person">
+              <LocalHospital className="icon" />
+              <h4>5</h4>
+              <h3>Nurses</h3>
+            </div>
+            <Link to="/ListPatients" style={{textDecoration: 'none'}}>
+            <div className="person">
+              <AirlineSeatIndividualSuite className="icon" />
+              <h4>10</h4>
+              <h3>Patients</h3>
+            </div>
+            </Link>
+          </div>
+          <div className="row3">
+            <div className="lower">
+              <LocalPharmacy className="icon" />
+              <h3>Store<br/>Details</h3>
+            </div>
+            <div className="lower access">
+              <div className="access-header">
+                <Star className="icon" />
+                <h3>Quick Access</h3>
+              </div>
+              <ul>
+                <Link to="/AddPatients" style={{textDecoration: 'none'}}>
+                <li>Register a new Patient</li>
+                </Link>
+                <li>Laboratory Test Results</li>
+                <li>Appointment with the Doctor</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
