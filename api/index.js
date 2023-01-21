bf06ddc1aea5149bfd49bb19c2200bd897eaaefc
nandweza <allannandweza@gmail.com>
2023-01-21 15:18:43 +0300

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const port = 3001;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB Connection successfully!"))
+.catch((err) => console.log(err));
 
 app.listen(port, () => {
     console.log(`app is listening on port: ${port}`);
